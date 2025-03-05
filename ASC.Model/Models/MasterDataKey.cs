a977bdb5a1d28710af976865073693cd9598ab7f
Pham Nghi <nghipham2003@gmail.com>
2025-03-05 22:49:02 +0700

    conenect database

---
+++ ASC.Model/Models/MasterDataKey.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASC.Model.BaseTypes;
+
+namespace ASC.Model.Models
+{
+    public class MasterDataKey: BaseEntity
+    {
+        public MasterDataKey() { }
+        public MasterDataKey(string key)
+        {
+            this.RowKey = Guid.NewGuid().ToString();
+            this.PartitionKey = key;
+        }
+        public string IsActive { get; set; }
+        public string Name { get; set; }
+
+    }
+}
